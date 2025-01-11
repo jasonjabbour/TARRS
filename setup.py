bea0dde6b94bd2464c67edd1ae498e372c6cf6ad
jasonjabbour <55008744+jasonjabbour@users.noreply.github.com>
2025-01-11 01:23:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='safe_ptp',
     version='0.1.0',
-    description='Safe PTP',
+    description='TARRS',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Jason Jabbour',
     author_email='jasonjabbour@g.harvard.edu',
-    url='https://github.com/jasonjabbour/safe_ptp',
+    url='https://github.com/jasonjabbour/TARRS',
     packages=find_packages(),
     python_requires='>=3.7',
 )
